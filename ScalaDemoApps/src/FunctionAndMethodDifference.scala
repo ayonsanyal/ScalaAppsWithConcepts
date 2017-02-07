71d6ac63c9b767cf9c71e0bb339e326895999414
ayonsanyal <ayon.sanyal@gmail.com>
2017-02-07 18:54:19 +0530

    Second Commit

---
+++ ScalaDemoApps/src/FunctionAndMethodDifference.scala
@@ -0,0 +1,22 @@
+
+
+/**
+ * @author AYON SANYAL
+ *
+ */
+object FunctionAndMethodDifference {
+  
+    
+    /**
+     * @return
+     */
+    def resultMethod():Double = {
+      
+      var result = (input1:Double,input2:Double)=>
+    {
+      input1*input2
+    }
+     0.0 
+    }
+    
+}
